ea3f53dae97166662e10878105e5fa9861993a8f
unknown <aaradhya.korde@gmail.com>
2025-04-06 13:05:17 +0530

    m

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const addGoogleTranslateScript = () => {
+      const script = document.createElement("script");
+      script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+      document.body.appendChild(script);
+
+      window.googleTranslateElementInit = () => {
+        new window.google.translate.TranslateElement(
+          { pageLanguage: 'en', includedLanguages: 'en,hi' },
+          'google_translate_element'
+        );
+      };
+    };
+
+    addGoogleTranslateScript();
+  }, []);
+
   return <Component {...pageProps} />;
 }
